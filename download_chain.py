169d4175e70fb58b1eb7fd68039c33df0ab35fb4
Ethan Fast <Ejhfast@gmail.com>
2017-07-15 14:36:32 -0700

    iter up to bloc count

--- download_chain.py
+++ download_chain.py
@@ -1,8 +1,23 @@
 import requests
 import json
+import sys
 
 nodeAPI = "http://api.otcgo.cn:10332"
 
-response = requests.post(nodeAPI, json={"jsonrpc": "2.0", "method": "getblock", "params": [1100000, 1], "id": 0})
+def rpcRequest(method, params):
+    return requests.post(nodeAPI, json={"jsonrpc": "2.0", "method": method, "params": params, "id": 0}).json()
 
-print(json.dumps(response.json()))
+def getBlock(index):
+    return rpcRequest("getblock", [index,1])
+
+def getBlockCount():
+    return rpcRequest("getblockcount", [])
+
+currentBlock = getBlockCount()["result"]
+
+with open(sys.argv[1], "w") as f:
+    for i in range(0, currentBlock):
+        if i % 10 == 0:
+            print(i)
+        nextBlock = getBlock(i)
+        f.write(json.dumps(nextBlock)+"\n")
